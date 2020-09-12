899593c0827ccb12178aa98b3b65e358c58851f8
Aleksandr Bobrovitskii <shmoxandco@mail.ru>
2020-09-12 13:29:21 +0300

    Home_work_2 Hashmap realization

---
+++ src/lesson_2/Main.java
@@ -0,0 +1,43 @@
+package lesson_2;
+
+import java.util.Iterator;
+
+/**
+ * Спроектировать и реализовать класс, реализующий коллекцию типа "хеш-таблица" (hash-map),
+ * или двунаправленная хеш-таблица (bi-map), без использования стандартных или библиотечных
+ * реализаций коллекций (можно использовать массивы). В качестве ключей и значений могут
+ * использоваться любые объекты. В коллекции должны быть методы добавления, обновления,
+ * получения, удаления элемента, проверка наличия элемента по ключу (и по значению в случае
+ * двунаправленной таблицы), получение количества элементов в коллекции. В случае совершения
+ * недопустимых операций выбрасывать исключения. Протестировать работоспособность с помощью
+ * юнит-тестов, включая положительные (при корректных операциях с коллекцией она ведет себя
+ * корректно), и негативные тесты (исключения при некоректных операциях).
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * Create and fill SimpleHashMap.
+         */
+        SimpleHashMap simpleHashMap = new SimpleHashMap();
+        simpleHashMap.put(1, "Apple");
+        simpleHashMap.put(2, "Banana");
+        simpleHashMap.put(3, "Cherry");
+
+        /**
+         * Display SimpleHashMap methods: size, get, toString, remove.
+         */
+        System.out.println("HashMap size: " + simpleHashMap.size());
+        System.out.println("HashMap get element dy key " + simpleHashMap.get(2));
+        System.out.println(simpleHashMap.toString());
+        simpleHashMap.remove(1);
+        /**
+         * Display iterator opportunities
+         */
+        Iterator<SimpleEntry> itr = simpleHashMap.iterator();
+        while (itr.hasNext()) {
+            SimpleEntry simpleEntry = itr.next();
+            System.out.println("Key: " + simpleEntry.getKey() + " Value: " + simpleEntry.getValue());
+        }
+    }
+}
---
+++ src/lesson_2/SimpleHashMap.java
@@ -0,0 +1,263 @@
+package lesson_2;
+
+import java.util.*;
+
+
+public class SimpleHashMap<K, V> implements Iterable<SimpleEntry<K, V>> {
+
+    private int capacity = 16;
+    private int size;
+    private int modificationCount;
+
+    @SuppressWarnings("unchecked")
+    private final SimpleEntry<K, V>[] array = (SimpleEntry<K, V>[]) new SimpleEntry[capacity];
+
+    public SimpleHashMap() {
+    }
+
+    public SimpleHashMap(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getModificationCount() {
+        return modificationCount;
+    }
+
+    /**
+     * Put into SimpleHashMAp method
+     *
+     * @param key   key param
+     * @param value value param
+     */
+    public void put(K key, V value) {
+        modificationCount++;
+        if (key == null) {
+            if (array[0] == null) {
+                array[0] = new SimpleEntry<K, V>(null, value);
+                size++;
+            } else {
+                array[0].setValue(value);
+            }
+        }
+
+        int numBacked = Math.abs(key.hashCode()) % capacity;
+
+
+        if (array[numBacked] == null) {
+            array[numBacked] = new SimpleEntry<K, V>(key, value);
+            size++;
+        } else {
+            SimpleEntry<K, V> entry = array[numBacked];
+
+            while (true) {
+                if (entry.getKey().equals(key)) {
+                    entry.setValue(value);
+                    return;
+                }
+
+                if (entry.getNext() == null) {
+                    break;
+                }
+                entry = entry.getNext();
+            }
+
+            entry.setNext(new SimpleEntry<K, V>(key, value));
+            size++;
+        }
+    }
+
+    /**
+     * Get from SimpleHashMAp method
+     *
+     * @param key seek by key param
+     * @return Value associated with key
+     */
+    public V get(K key) {
+
+        if (key == null) {
+            if (array[0] == null) {
+                return null;
+            }
+
+            return array[0].getValue();
+        }
+
+        int numBacked = Math.abs(key.hashCode()) % capacity;
+
+        if (array[numBacked] == null) {
+            return null;
+        } else {
+            SimpleEntry<K, V> entry = array[numBacked];
+
+            while (true) {
+                if (entry.getKey().equals(key)) {
+                    return entry.getValue();
+                }
+
+                if (entry.getNext() == null) {
+                    return null;
+                }
+
+                entry = entry.getNext();
+            }
+        }
+    }
+
+    /**
+     * Remove form SimpleHashMap method
+     *
+     * @param key by key param
+     */
+    public void remove(K key) {
+        if (key == null) {
+            if (array[0] != null) {
+                modificationCount++;
+                size--;
+
+                if (array[0].getNext() != null) {
+                    array[0] = array[0].getNext();
+                }
+            }
+            return;
+        }
+
+        int numBacked = Math.abs(key.hashCode()) % capacity;
+
+        if (array[numBacked] != null) {
+            if (array[numBacked].getKey().equals(key)) {
+                array[numBacked] = array[numBacked].getNext();
+                modificationCount++;
+                size--;
+                return;
+            }
+
+            if (array[numBacked].getNext() != null) {
+                SimpleEntry<K, V> prev = array[numBacked];
+                SimpleEntry<K, V> entry = array[numBacked].getNext();
+                SimpleEntry<K, V> next = entry.getNext();
+
+                while (true) {
+                    if (entry.getKey().equals(key)) {
+                        modificationCount++;
+                        size--;
+
+                        prev.setNext(next);
+                        return;
+                    }
+
+                    if (next == null) {
+                        return;
+                    }
+
+                    prev = entry;
+                    entry = next;
+                    next = next.getNext();
+                }
+            }
+        }
+    }
+
+    /**
+     * Size method
+     *
+     * @return SimpleHashMAp size
+     */
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<SimpleEntry<K, V>> iterator() {
+
+        return new Iterator<SimpleEntry<K, V>>() {
+
+            private int expectedModificationCount;
+            private int currentNumBacked;
+            private SimpleEntry<K, V> nextEntry;
+            private SimpleEntry<K, V> currentEntry;
+
+            {
+                expectedModificationCount = modificationCount;
+                for (SimpleEntry<K, V> entry : array) {
+                    if (entry != null) {
+                        nextEntry = entry;
+                        break;
+                    }
+                }
+            }
+
+            @Override
+            public boolean hasNext() {
+                return nextEntry != null;
+            }
+
+            @Override
+            public SimpleEntry<K, V> next() {
+                if (modificationCount != expectedModificationCount) {
+                    throw new ConcurrentModificationException();
+                }
+                currentEntry = nextEntry;
+
+                if (nextEntry.getNext() == null) {
+                    nextEntry = null;
+                    for (++currentNumBacked; currentNumBacked < capacity; currentNumBacked++) {
+                        if (array[currentNumBacked] != null) {
+                            nextEntry = array[currentNumBacked];
+                        }
+                    }
+                } else {
+                    nextEntry = nextEntry.getNext();
+                }
+
+                return currentEntry;
+            }
+
+            public void remove() {
+                if (modificationCount != expectedModificationCount) {
+                    throw new ConcurrentModificationException();
+                }
+
+                if (currentEntry == null) {
+                    throw new UnsupportedOperationException();
+                }
+
+                SimpleHashMap.this.remove(currentEntry.getKey());
+                expectedModificationCount++;
+            }
+
+        };
+    }
+
+    public Set<SimpleEntry<K, V>> entrySet() {
+        Set<SimpleEntry<K, V>> set = new HashSet<>();
+
+        for (SimpleEntry<K, V> entry : array) {
+            while (entry != null) {
+                set.add(entry);
+                entry = entry.getNext();
+            }
+        }
+
+        return set;
+    }
+
+    public Set<K> keySet() {
+        Set<K> set = new HashSet<>();
+
+        for (SimpleEntry<K, V> entry : array) {
+            while (entry != null) {
+                set.add(entry.getKey());
+                entry = entry.getNext();
+            }
+        }
+
+        return set;
+    }
+
+    @Override
+    public String toString() {
+        return "SimpleHashMap{" +
+                "array=" + Arrays.toString(array) +
+                '}';
+    }
+}
