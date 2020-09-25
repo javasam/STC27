e382eadad35176743ce5447577af2387d3417451
Aleksandr Bobrovitskii <shmoxandco@mail.ru>
2020-09-25 15:07:30 +0300

    lesson_12 Memory leak

---
+++ src/lesson_12/Home_work_12/OutOfMemoryMeta.java
@@ -0,0 +1,14 @@
+package lesson_12.Home_work_12;
+
+/**
+ * A simple class that creates a potential memory leak, but you need to wait a long time
+ */
+
+public class OutOfMemoryMeta extends Thread {
+
+    public void run() {
+        while (true) {
+            new StringFiller();
+        }
+    }
+}
--- src/lesson_12/Home_work_12/TestClass.java
+++ src/lesson_12/Home_work_12/TestClass.java
@@ -2,15 +2,35 @@ package lesson_12.Home_work_12;
 
 import org.junit.Test;
 
+/**
+ * 1. Необходимо создать программу, которая продемонстрирует утечку памяти в Java.
+ * При этом объекты должны не только создаваться, но и периодически частично удаляться,
+ * чтобы GC имел возможность очищать часть памяти. Через некоторое время программа должна
+ * завершиться с ошибкой OutOfMemoryError c пометкой Java Heap Space.
+ * <p>
+ * 2. Сделать чтобы ошибка OutOfMemoryError была в Permanent Generation (или Metaspace)
+ */
+
 public class TestClass {
 
     @Test
-    public void whenStartMethodFiller() {
+    public void whenStartMethodOutOFMemoryHeap() {
         StringFiller stringFiller = new StringFiller();
         try {
-            stringFiller.outOfMemory(Long.MAX_VALUE);
+            stringFiller.outOfMemoryHeapSpace(Integer.MAX_VALUE);
         } catch (OutOfMemoryError e) {
+            e.printStackTrace();
             System.out.println("Test pass!!!");
         }
     }
+
+    @Test
+    public void whenStartMethodOutOfMemoryMeta() {
+        for (int i = 0; i < 5; i++) {
+            new OutOfMemoryMeta().start();
+            System.out.println("Memory leak started: " + i);
+        }
+    }
+
+
 }
